6a19ba8afd431905df31c05a5266070f6f0d1071
Vladimir Sumarov <summeroff@gmail.com>
2019-05-23 08:54:35 -0700

    add command line option for blocking timeout

--- file_blocker.cpp
+++ file_blocker.cpp
@@ -1,27 +1,52 @@
 // file_blocker.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
- 
+
 #include <iostream>
 #include <string>
 #include <Windows.h>
 
 bool use_extended_locking = false;
+int  locking_time_out = 30;
 
 int main(int argc, char * argv[])
 {
-	HANDLE h_file = 0;	
+	HANDLE h_file = 0;
 	BOOL lock_success = false;
-	std::cout << "Hello there! Gonna lock this file\n";
 
-	if (argc >= 2)
+	//get options 
+	int checked_params = 1;
+	while (argc > checked_params)
 	{
-		std::string first_param(argv[1]);
-		if (first_param.compare("-l") == 0)
+		std::string param_name(argv[checked_params]);
+		if (param_name.compare("-l") == 0)
 		{
 			use_extended_locking = true;
+			checked_params += 1;
+			continue;
+		}
+		else if (param_name.compare("-t") == 0)
+		{
+			if (argc >= checked_params + 1)
+			{
+				std::string param_value(argv[checked_params + 1]);
+				try
+				{
+					locking_time_out = std::stoi(param_value);
+					checked_params += 2;
+					continue;
+				}
+				catch (...) {}
+			}
+			checked_params += 1;
+			continue;
 		}
 	}
-	
+
+	//to locking 
+	std::cout << "Hello there! Gonna lock this file for " << locking_time_out << "sec \n";
+
+	OVERLAPPED overlapvar = { 0 };
+
 	if (use_extended_locking)
 	{
 		wchar_t app_path_buffer[MAX_PATH + 1];
@@ -34,8 +59,6 @@ int main(int argc, char * argv[])
 		std::wstring app_path_wstring = app_path_buffer;
 		std::cout << "Run file: \"" << std::string(app_path_wstring.begin(), app_path_wstring.end()) << "\" (only block writing and deleting )\n";
 
-		OVERLAPPED overlapvar = { 0 };
-		
 		std::cout << "Lock file: \"" << std::string(app_path_wstring.begin(), app_path_wstring.end()) << "\" (block reading too)\n";
 
 		h_file = CreateFile(app_path_buffer, GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -47,19 +70,27 @@ int main(int argc, char * argv[])
 				std::cout << "Error: Failed to lock file\n";
 			}
 		}
-		else {
+		else
+		{
 			std::cout << "Error: Failed to lock file\n";
 		}
 	}
 
-	while (1)
+	//wait 
+	int wait_steps = locking_time_out;
+
+	while (wait_steps--)
 	{
-		Sleep(500);
+		Sleep(1000);
 		std::cout << "Still blocking file: \"" << argv[0] << "\"\n";
 	}
 
-	CloseHandle(h_file);
+	//unlock and exit 
+	if (use_extended_locking)
+	{
+		UnlockFileEx(h_file, 0, MAXDWORD, MAXDWORD, &overlapvar);
+		CloseHandle(h_file);
+	}
 
 	return 0;
 }
- 
